61262880c534cc2b7cfbd20ea38b0d3d821ab28c
Morganero <morganero1@gmail.com>
2025-04-30 11:02:09 -0500

    fixed search layout shift

--- src/ui/SearchResults.jsx
+++ src/ui/SearchResults.jsx
@@ -1,15 +1,23 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Heading from './Heading';
 import Show from './Show';
 import ShowList from './ShowList';
 import { useSearchParams } from 'react-router-dom';
+
+const ResultsContainer = styled.div`
+  min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+`;
 
 function SearchResults({ results }) {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query');
 
   return (
-    <>
+    <ResultsContainer>
       <Heading as="h1">
         Found {results.length} {results.length > 1 ? 'results' : 'result'} for{' '}
         &apos;{query}&apos;
@@ -22,7 +30,7 @@ function SearchResults({ results }) {
           />
         ))}
       </ShowList>
-    </>
+    </ResultsContainer>
   );
 }
 
--- src/ui/ShowList.jsx
+++ src/ui/ShowList.jsx
@@ -2,17 +2,24 @@ import styled from 'styled-components';
 
 const ShowList = styled.ul`
   display: grid;
-  grid-template-columns: repeat(2, minmax(16.4rem, 1fr));
+  grid-template-columns: repeat(2, 1fr);
   justify-content: center;
   gap: 1.6rem;
+  width: 100%;
+  max-width: 100%;
+  margin: 0 auto;
+
+  & > * {
+    min-width: 0; /* Prevents grid item overflow */
+  }
 
   @media (min-width: 48em) {
-    grid-template-columns: repeat(3, minmax(22rem, 1fr));
+    grid-template-columns: repeat(3, 1fr);
     gap: 3rem;
   }
 
   @media (min-width: 90em) {
-    grid-template-columns: repeat(4, minmax(28rem, 1fr));
+    grid-template-columns: repeat(4, 1fr);
     gap: 4rem;
   }
 `;
